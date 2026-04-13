734fd95e30f8a598c7b00f2c8e3fac36b4c6495a
AshKmo <ashkmo33@gmail.com>
2026-04-13 21:49:53 +1000

    generate_problems.py documentation

--- generate_problems.py
+++ generate_problems.py
@@ -1,7 +1,12 @@
+# This script uses the problem generator ("problem_generator.py") to quickly generate a large number of random tests at once
+
+# the number of tests to generate
+n = 100
+
 from problem_generator import generate_problem
 
 from test_file_lib import to_test_file
 
-for i in range(0, 100):
+for i in range(0, n):
     with open(f"tests/generated/test-generated-{i}.txt", "w") as f:
         f.write(to_test_file(*generate_problem()))
